1c938c427920b505517bf1c82995931a08a8cf02
Jatinder <jsingh318@gmail.com>
2020-02-03 03:19:11 +0000

    artist route setup

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const artistControllers = require('./controllers/artist');
-const albumControllers = require('./controllers/album');
-const songControllers = require('./controllers/song');
+// const albumControllers = require('./controllers/album');
+// const songControllers = require('./controllers/song');
 
 const app = express();
 app.use(express.json());
 
 app.post('artists/', artistControllers.create);
-app.post('albums/', albumControllers.create);
-app.post('songs/', songControllers.create);
+// app.post('albums/', albumControllers.create);
+// app.post('songs/', songControllers.create);
 
 module.exports = app;
