75251ec1586753411b0a40a34046b1cf6e376c9e
Jatinder <jsingh318@gmail.com>
2020-02-06 16:23:25 +0000

    album test pass

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const artistControllers = require('./controllers/artist');
-// const albumControllers = require('./controllers/album');
+const albumControllers = require('./controllers/album');
 // const songControllers = require('./controllers/song');
 
 const app = express();
@@ -11,5 +11,9 @@ app.get('/artists', artistControllers.list);
 app.get('/artists/:artistId', artistControllers.find);
 app.patch('/artists/:artistId', artistControllers.update);
 app.delete('/artists/:artistId', artistControllers.delete);
+app.post('/artists/:artistId/albums', albumControllers.create);
+app.get('/albums', albumControllers.list);
+app.patch('/artists/:artistId/albums/:albumId', albumControllers.update);
+app.delete('/artists/:artistId/albums/albumId', albumControllers.delete);
 
 module.exports = app;
--- src/controllers/album.js
+++ src/controllers/album.js
@@ -1,3 +1,46 @@
+const Album = require('../models/album');
+
 exports.create = (req, res) => {
-  res.sendStatus(201);
+  const { artistId } = req.params;
+  const album = new Album({
+    name: req.body.name,
+    year: req.body.year,
+    artist: artistId,
+  });
+  if (!album.artist) {
+    res.status(404).json({ error: 'The artist could not be found.' });
+  } else {
+    album.save().then(() => {
+      res.status(201).json(album);
+    });
+  }
+};
+
+exports.list = (req, res) => {
+  Album.find({}, (err, data) => {
+    res.status(200).json(data);
+  });
+};
+
+exports.update = (req, res) => {
+  Album.findById({ _id: req.params.albumId }, (err, data) => {
+    if (!data) {
+      res.status(404).json({ error: '44The album could not be found.' });
+    } else {
+      data.set(req.body);
+      data.save().then(updatedData => {
+        res.status(200).json(updatedData);
+      });
+    }
+  });
+};
+
+exports.delete = (req, res) => {
+  Album.findByIdAndDelete({ _id: req.params.albumId }, (err, data) => {
+    if (!data) {
+      res.status(404).json({ error: 'The artist could not be found.' });
+    } else {
+      res.status(204).json({ Message: 'Album was deleted' });
+    }
+  });
 };
